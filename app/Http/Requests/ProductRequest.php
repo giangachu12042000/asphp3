f6e0894dac056d5a9f4cdf6fabf02958c8208075
su ho <subhawj95@gmail.com>
2020-10-21 19:53:28 +0700

    comment fix

--- app/Http/Requests/ProductRequest.php
+++ app/Http/Requests/ProductRequest.php
@@ -6,10 +6,23 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class ProductRequest extends FormRequest
 {
+    public function attributes()
+    {
+        return [
+            'first_name' => 'Họ',
+            'last_name' => 'Tên',
+            'password' => 'Mật khẩu',
+            'image_url' => 'Ảnh đại diện',
+            'email' => 'Email',
+            'birthday' => 'Ngày sinh',
+            'address' => 'Địa chỉ'
+        ];
+    }
+
     public function rules()
     {
         return [
-            'name' => 'required',
+            'name' => 'required | max:20 | min:2',
             'category_id' => 'required',
             'image_url' => 'image',
             'price' => 'required',
@@ -24,7 +37,9 @@ class ProductRequest extends FormRequest
         return [
             'required' => ':attribute không được để trống',
             'unique' => ':attribute đã tồn tại',
-            'image' => ':attribute không đúng định dạng'
+            'image' => ':attribute không đúng định dạng',
+            'max' => ':attribute độ dài tối đa không vượt quá 20 ký tự',
+            'min' => ':attribute độ dài tối thiểu là 2 ký tự'
         ];
     }
 }
